40c278238bbc97da9ffafd77a6f408219748854e
Pedro Henrique dos Santos <pedro.dsantos@sptech.school>
2026-05-19 20:39:28 -0300

    ATUALIZADO MODELS

--- js/mapaModel.js
+++ js/mapaModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config");
 
-// 1. Calcula a porcentagem de Fav e Ban baseada no total de votos de cada mapa
+ 
 function listarVotosPorcentagem() {
     console.log("ACESSEI O MAPA MODEL \n \t\t >> Função: listarVotosPorcentagem");
     
@@ -9,7 +9,7 @@ function listarVotosPorcentagem() {
             fkMapa,
             ROUND((COUNT(CASE WHEN tipoVoto = 'Favorito' THEN 1 END) / COUNT(*)) * 100) AS porcentagemFavorito,
             ROUND((COUNT(CASE WHEN tipoVoto = 'Banir' THEN 1 END) / COUNT(*)) * 100) AS porcentagemBan
-        FROM votacao
+        FROM voto
         GROUP BY fkMapa;
     `;
     
@@ -17,12 +17,12 @@ function listarVotosPorcentagem() {
     return database.executar(instrucaoSql);
 }
 
-// 2. Salva o voto (idUsuario, idMapa e se foi 'Favorito' ou 'Banir')
+
 function votar(idUsuario, idMapa, tipoVoto) {
     console.log("ACESSEI O MAPA MODEL \n \t\t >> Função: votar");
     
     var instrucaoSql = `
-        INSERT INTO votacao (fkUsuario, fkMapa, tipoVoto) VALUES (${idUsuario}, ${idMapa}, '${tipoVoto}');
+        INSERT INTO voto (fkUsuario, fkMapa, tipoVoto) VALUES (${idUsuario}, ${idMapa}, '${tipoVoto}');
     `;
     
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
