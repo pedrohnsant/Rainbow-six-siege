d06db2d7ec8f79dbd1d67b0bef55c14437bf5df0
Pedro Henrique dos Santos <pedro.dsantos@sptech.school>
2026-05-19 20:31:00 -0300

    Script atualizado, alteração de nome de tabela
    tabela "votacao" renomeada para "voto

--- database/script-tabelas.sql
+++ database/script-tabelas.sql
@@ -1,13 +1,6 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE r6_forum;
 USE r6_forum;
+
 CREATE TABLE usuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
@@ -25,27 +18,57 @@ CREATE TABLE resultado_dashboard (
     CONSTRAINT fk_usuario_resultado FOREIGN KEY (fkUsuario) 
         REFERENCES usuario(id)
 );
+ 
+ALTER TABLE resultado_dashboard 
+ADD COLUMN mes_ano_controle VARCHAR(7);
 
 ALTER TABLE resultado_dashboard 
+ADD CONSTRAINT UNIQUE_usuario_mes UNIQUE (fkUsuario, mes_ano_controle);
+
+ ALTER TABLE resultado_dashboard 
 ADD COLUMN data_registro DATETIME DEFAULT CURRENT_TIMESTAMP;
 
 CREATE TABLE mapa (
     idMapa INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
-    tipo VARCHAR(45), -- Ex: Competitivo, Retrabalho
+    tipo VARCHAR(45),  
     imagemUrl VARCHAR(255),
     descricao TEXT
 );
 
--- 2. Tabela Intermediária: Votacao (Relacionamento Muitos para Muitos)
-CREATE TABLE votacao (
+ 
+CREATE TABLE voto (
     idVotacao INT AUTO_INCREMENT,
     fkUsuario INT,
     fkMapa INT,
+    tipoVoto VARCHAR(20) NOT NULL,  
     dataVoto DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (idVotacao, fkUsuario, fkMapa),
-    CONSTRAINT fk_votacao_usuario FOREIGN KEY (fkUsuario) 
-        REFERENCES usuario(id), 
-    CONSTRAINT fk_votacao_mapa FOREIGN KEY (fkMapa) 
-        REFERENCES mapa(idMapa)
+    CONSTRAINT fk_votacao_usuario FOREIGN KEY (fkUsuario) REFERENCES usuario(id), 
+    CONSTRAINT fk_votacao_mapa FOREIGN KEY (fkMapa) REFERENCES mapa(idMapa)
 );
+
+INSERT INTO mapa (idMapa, nome, tipo, imagemUrl, descricao) VALUES
+(1, 'Clube', 'Competitivo', 'clubhouse.jpg', 'Briefing tático do mapa Clube.'),
+(2, 'Oregon', 'Competitivo', 'oregon.jpg', 'Briefing tático do mapa Oregon.'),
+(3, 'Chalé', 'Competitivo', 'chalet.jpg', 'Briefing tático do mapa Chalé.'),
+(4, 'Consulado', 'Competitivo', 'consulado.jpg', 'Briefing tático do mapa Consulado.'),
+(5, 'Banco', 'Competitivo', 'banco.jpg', 'Briefing tático do mapa Banco.'),
+(6, 'Canal', 'Competitivo', 'canal.jpg', 'Briefing tático do mapa Canal.'),
+(7, 'Litoral', 'Competitivo', 'litoral.jpg', 'Briefing tático do mapa Litoral.'),
+(8, 'Café Dostoyevsky', 'Competitivo', 'cafe.jpg', 'Briefing tático do mapa Café.'),
+(9, 'Parque Temático', 'Competitivo', 'parque.jpg', 'Briefing tático do mapa Parque.'),
+(10, 'Fronteira', 'Competitivo', 'fronteira.jpg', 'Briefing tático do mapa Fronteira.'),
+(11, 'Outback', 'Competitivo', 'outback.jpg', 'Briefing tático do mapa Outback.'),
+(12, 'Arranha-céu', 'Competitivo', 'arranha.jpg', 'Briefing tático do mapa Arranha-céu.'),
+(13, 'Fortaleza', 'Competitivo', 'fortaleza.jpg', 'Briefing tático do mapa Fortaleza.'),
+(14, 'Villa', 'Competitivo', 'vila.jpg', 'Briefing tático do mapa Villa.'),
+(15, 'Laboratório Nighthaven', 'Competitivo', 'laboratorio.jpg', 'Briefing tático do mapa Nighthaven.'),
+(16, 'Covil', 'Competitivo', 'covil.jpg', 'Briefing tático do mapa Covil.');
+
+SELECT * FROM usuario;
+SELECT * FROM voto;
+SELECT * FROM resultado_dashboard;
+describe resultado_dashboard;
+
+ 
